137c633698cec1dc013695ed0055dc64f8c6b51a
Cristian-droi <cristian.eduardo.morales@correounivalle.edu.co>
2022-09-11 23:39:01 -0500

    1 commit

---
+++ main.js
@@ -0,0 +1,39 @@
+import data from "./data.json" assert { type: "json" };
+
+let chartBarsContainer = document.querySelector(".chart__bars-container");
+
+let values = [];
+
+data.forEach((element) => {
+  values.push(element.amount);
+  chartBarsContainer.innerHTML += `
+    <div class="chart__bar">
+      <div class="chart__bar--label">$${element.amount}</div>
+      <div class="chart__bar--day">${element.day}</div>
+    </div>`;
+});
+console.log(values);
+
+let maxValues = Math.max(...values);
+let alturaMaxBarPx = 150;
+let bars = document.querySelectorAll(".chart__bar");
+bars = [...bars];
+
+bars.forEach((bar) => {
+  let nuevoValor = parseFloat(bar.childNodes[1].innerText.slice(1));
+  let alturaActualPx = (nuevoValor * alturaMaxBarPx) / maxValues;
+  bar.style.height = `${alturaActualPx}px`;
+
+  if (nuevoValor == maxValues) {
+    bar.style.backgroundColor = "hsl(186, 34%, 60%)";
+  }
+
+  bar.addEventListener("mouseover", (event) => {
+    let labelElement = event.target.childNodes[1];
+    labelElement.style.display = "block";
+  });
+  bar.addEventListener("mouseout", (event) => {
+    let labelElement = event.target.childNodes[1];
+    labelElement.style.display = "none";
+  });
+});
